1781cc3a5faca37a9edcce65fa3981ce136a763e
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-09-22 02:28:10 -0500

    Added lightning support

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 	log_directory = 'logs'
 	model_name = 'umls-kbilm-v2'
 	pre_model_name = 'monologg/biobert_v1.1_pubmed'
-	batch_size = 32
+	batch_size = 16
 	negative_sample_size = 16
 	weight_decay = 0.01
 	learning_rate = 1e-5
